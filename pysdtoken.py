36a0dbd2fa35bf91e52f9f5db99dbf77a0b825b3
XanaDublaKublaConch <xanadu@styrophobia.org>
2019-03-12 16:18:00 -0400

    Add platform checking so it works on both Windows and Mac (requires platform module.) Added TokenError enum for error reporting. Corrected typo in "get_expiration_dat". Used handle value for close_service() instead of the handle object. Consistently return a c_long for count_tokens() instead of returning empty string on failure. Encoded pin before dll call in get_token_current_code() Implemented dll GetTokenError via SDProcess.get_token_error(). Used token error enum for feedback in various functions when failed.

--- pysdtoken.py
+++ pysdtoken.py
@@ -3,8 +3,10 @@ Provide python access to th eRSA Soft Token Service
 This module wraps the stauto32.dll calls using
 ctypes to get the current code from the token
 """
-
+import platform
 from ctypes import *
+from enum import Enum
+
 
 # A struct to hold token error information
 class struct_tagTOKENERRORINFO(Structure):
@@ -33,6 +35,66 @@ class struct_tagTOKENBASICINFO(Structure):
 
 token_basic_info = struct_tagTOKENBASICINFO
 
+# Enum for Errors
+class TokenError(Enum):
+    ERROR_DISPLAY_STRING = -1
+    ERROR_INIT = 1
+    ERROR_INVALID_PNTR = 1
+    ERROR_TIME = 2
+    ERROR_DLL_LOADED = 2
+    ERROR_SOFTID_CANNOTRUN = 2
+    ERROR_LOAD_DLL_ERROR = 3
+    ERROR_SECURITY = 3
+    ERROR_SOFTID_DEAD = 3
+    ERROR_RESOURCE_ERROR = 4
+    ERROR_READ_REG_ERROR = 5
+    ERROR_WRITE_REG_ERROR = 6
+    ERROR_INVALID_SET=NUMBER = 7
+    ERROR_PROC_ADDRESS_ERROR = 8
+    ERROR_INVALID_NAME_ERROR = 9
+    ERROR_DLL_CALL_ERROR = 10
+    ERROR_INACTIVE_DLL = 11
+    ERROR_DATA_INVALID = 12
+    ERROR_HANDLE_INVALID = 13
+    ERROR_KEY_INVALID = 14
+    ERROR_TOKEN_INVALID = 15
+    ERROR_TOKEN_NOTFOUND = 16
+    ERROR_BUFFER_TOO_SMALL_ERROR = 17
+    ERROR_TOKEN_SERVICE_OPEN = 18
+    ERROR_INVALID_COMP_NAME = 19
+    ERROR_SET_ONE_DELETED = 20
+    ERROR_PASSWORD_INVALID = 21
+    ERROR_NONE_MATCHING_PASSWORD = 22
+    ERROR_COPY_PROTECTION_DEVICE = 23
+    ERROR_COPY_PROTECTION_TOKEN = 24
+    ERROR_TOKENCODE_GENERATION = 25
+    ERROR_DATABASE = 26
+    ERROR_LOAD_LIBRARY = 27
+    ERROR_OPEN_FILE = 28
+    ERROR_OPEN_DATABASE = 29
+    ERROR_SCARD = 30
+    ERROR_P_ELEVEN = 31
+    ERROR_PLUGIN = 32
+    ERROR_SDD = 33
+    ERROR_PSD = 34
+    ERROR_DEVICE_LOGIN = 35
+    ERROR_SELECT_TOKEN_SERIAL = 36
+    ERROR_DEVICE_BINDING = 37
+    ERROR_GUID_INVALID = 38
+    ERROR_PLUGIN_IMPLEMENTATION = 39
+    ERROR_FILE_FORMAT = 40
+    ERROR_INET_COMMUNICATION = 41
+    ERROR_TOKENFILE_PASSWORD_CHECK = 42
+    ERROR_SERVER_BAD_STATUS = 43
+    ERROR_DUPLICATE_SERIAL = 44
+    ERROR_SSL_UNTRUSTED_CERT = 45
+    ERROR_DEVICE_FULL = 46
+    ERROR_DEVICE_RESET = 47
+    ERROR_EXPIRED_TOKEN = 48
+    ERROR_PASSWORD_FAILURE = 216
+    ERROR_IMPORT_FAILURE = 217
+    ERROR_MAX_IMPORT_TOKENS = 49
+
 
 # A struct to hold dates
 class struct_CK_DATE(Structure):
@@ -60,7 +122,7 @@ class Token:
         self.descriptor = token_data['descriptor']
         self.is_default = token_data['is_default']
 
-    def get_expiration_dat(self, token_service):
+    def get_expiration_date(self, token_service):
         return token_service.get_token_expiration_date(self.serialnumber)
 
     def get_current_code(self, token_service, pin=''):
@@ -71,14 +133,22 @@ class SDProcess:
 
     tokens = []
     lTokens = c_long()
+    dll_name = 'stauto32.dll'
 
     def __init__(self):
         # Get the SD process
-        try:
-            self.process = cdll.LoadLibrary('stauto32.dll')
-        except Exception as e:
-            print(e)
-            print("Error finding Soft Token service.")
+        if platform.system() == 'Windows':
+            try:
+                self.process = windll.LoadLibrary(self.dll_name)
+            except Exception as e:
+                print(e)
+                print("Error finding Soft Token service.")
+        else:
+            try:
+                self.process = cdll.LoadLibrary(self.dll_name)
+            except Exception as e:
+                print(e)
+                print("Error finding Soft Token service.")
 
         self.handle = c_long()
         self.lDefaultToken = c_long()
@@ -88,6 +158,9 @@ class SDProcess:
         print("DEBUG: There are {} tokens.".format(self.lTokens.value))
         # Populate the token dict
         self.tokens = self.get_tokens()
+        for stoken in self.tokens:
+            print("{0}: {1}".format(stoken.serialnumber, "Default" if stoken.is_default else ""))
+
 
     def open_service(self):
         try:
@@ -103,10 +176,11 @@ class SDProcess:
     def close_service(self):
         try:
             # > 0 means success
-            if self.process.CloseTokenService(self.handle) > 0:
+            if self.process.CloseTokenService(self.handle.value) > 0:
                 print("DEBUG: Token Service closed")
             else:
                 print("Could not close token service.")
+                self.get_token_error()
         except Exception as e:
             print(e)
             print("Error closing service.")
@@ -128,7 +202,8 @@ class SDProcess:
         if self.lTokens.value > 0:
             return dwBuffersize
         else:
-            return ''
+            self.get_token_error()
+            return c_long(0)
 
     def get_tokens(self):
         # Get the number of tokens registered with the token service by enumerating. The lTokens
@@ -146,17 +221,19 @@ class SDProcess:
 
         # There are lTokens # of tokens. Get them in an array. The dwBuffersize has to have been set
         # previously, which is done during init in the count_tokens() call. If dwBuffer points to a
-        # null DWORD, the function will return zero/false and fill dwBuffersize withthe correct size
+        # null DWORD, the function will return zero/false and fill dwBuffersize with the correct size
         try:
             if self.process.EnumToken(self.handle, byref(self.lTokens), byref(self.lDefaultToken), byref(lpTokens),
                                       byref(self.dwBuffersize)) > 0:
                 print("{} tokens found:".format(self.lTokens.value))
             else:
                 print("Did not find any tokens.")
+                self.get_token_error()
 
         except Exception as e:
             print(e)
             print("Error getting tokens.")
+            self.get_token_error()
             return []
 
         # Grab the token basic info for each token into a Token array. All stauto32 strings are utf-8
@@ -182,15 +259,17 @@ class SDProcess:
     def get_token_current_code(self, serial, pin=''):
         # get the current code that would be displayed on the token screen
         # return a tuple of code + time-left
-        chPASSCODE = c_char_p("".encode("utf-8"))
-        chPRN = c_char_p("".encode("utf-8"))
+        chPASSCODE = c_char_p("one".encode("utf-8"))
+        chPRN = c_char_p("two".encode("utf-8"))
+        chPIN = c_char_p(pin.encode('utf-8'))
         lTimeLeft = c_long()
         try:
-            self.process.GetCurrentCode(self.handle, serial.encode("utf-8"), pin.encode("utf-8"),
+            self.process.GetCurrentCode(self.handle, serial.encode("utf-8"), chPIN,
                                         byref(lTimeLeft), chPASSCODE, chPRN)
         except Exception as e:
             print(e)
             print("Error getting token code.")
+            self.get_token_error()
         # On pinless tokens, PASSCODE and PRN will be the same
         return chPASSCODE.value.decode('utf-8'), chPRN.value.decode('utf-8'), lTimeLeft.value
 
@@ -208,10 +287,32 @@ class SDProcess:
                                                         bytes(eDate.year).decode('utf-8')
                                                         )
             else:
+                self.get_token_error()
                 token_expiration = None
         except Exception as e:
             print(e)
             print("Error getting token expiration date.")
+            self.get_token_error()
             token_expiration = None
 
         return token_expiration
+
+    def get_token_error(self):
+        # Get any token error. Create a TOKENERRORINFO struct
+        token_error = token_error_info()
+        # get a pointer to use in the dll call
+        lp_token_error = pointer(token_error)
+        # Call the dll function, pass in the struct pointer to get filled. > 0 is success
+        if self.process.GetTokenError(self.handle, lp_token_error) > 0:
+            if lp_token_error is not None and token_error is not None:
+                # Dereference the pointer/get contents
+                content = lp_token_error.contents
+                if content.error != 0:
+                    err_number = int(content.error)
+                    print("{0}: {1}".format(err_number, TokenError(err_number).name))
+                else:
+                    print("No errors reported.")
+            else:
+                print("Returned NULL pointer.")
+        else:
+            print("No token error content.")

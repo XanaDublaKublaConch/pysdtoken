1a0428ba4314c71cef92c5c8fe65c9b9878d1648
Johnny Birchett <johnny@birchetts.com>
2020-11-10 10:52:49 -0500

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysdtoken",
-    version="1.0.0",
+    version="1.0.1",
     author="XanaDublaKublaConch",
     author_email="XanaDublaKublaConch@styrophobia.org",
     description="Pythonic wrapper for the RSA sdauto32.dll soft token service",
